a70d47b9e84b04cc09552c50b168b2c63798a81c
aka-nick <bechalrang@gmail.com>
2023-03-14 15:19:52 +0900

    섹션 9 - 스프링 트랜잭션 이해 - InternalCallV1Test를 통한 프록시 내부 호출 주의사항 2 (항상 트랜잭션AOP 적용이 필요한 메서드는 외부 클래스로 분리(클래스 내부에서 메소드끼리 호출하지 않도록))

---
+++ src/test/java/hello/springtx/apply/IntenalCallV2Test.java
@@ -0,0 +1,87 @@
+package hello.springtx.apply;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class IntenalCallV2Test {
+
+    @Autowired
+    CallService service;
+
+
+    @Test
+    void printProxy() {
+        log.info("call servcie class ={}", service.getClass());
+    }
+
+//    @Test
+//    void internalCall() {
+//        service.internal();
+//    }
+
+    @Test
+    void externalCall() {
+        service.external();
+    }
+
+    @TestConfiguration
+    static class IntenalCallV1TestConfig {
+        @Bean
+        CallService callService() {
+            return new CallService(callInternalService());
+        }
+        @Bean
+        CallInternalService callInternalService() {
+            return new CallInternalService();
+        }
+    }
+
+    static class CallService {
+
+        private final CallInternalService internalService;
+
+        public CallService(CallInternalService internalService) {
+            this.internalService = internalService;
+        }
+
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internalService.internal();
+        }
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly = {}", readOnly);
+
+        }
+    }
+
+    // 트랜잭션 메서드는 외부 클래스로 분리한다.
+    static class CallInternalService {
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly = {}", readOnly);
+
+        }
+    }
+
+
+}
