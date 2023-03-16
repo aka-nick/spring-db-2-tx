cb0824b899c7c16e82e9d0e671c75eef887d54f2
aka-nick <bechalrang@gmail.com>
2023-03-16 11:14:29 +0900

    섹션 11 - 트랜잭션 전파 활용 - 테스트 케이스 추가(기본 케이스인 joinV1 동작 확인)

---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,40 @@
+package hello.springtx.propagation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * MemberService        @Transactional: OFF
+     * MemberRepository     @Transactional: ON
+     * LogRepository        @Transactional: ON
+     */
+    @Test
+    void outerTxOff_success() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+
+
+}
