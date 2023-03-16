8b6a6b8629b3ebf7dce4bf30548257c5e3d5e75e
aka-nick <bechalrang@gmail.com>
2023-03-16 11:02:13 +0900

    섹션 11 - 트랜잭션 전파 활용 - MemberService 생성, joinV2 구현(로그 예외 발생 시 정상흐름 반환)

--- src/main/java/hello/springtx/propagation/MemberService.java
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -25,4 +25,23 @@ public class MemberService {
         log.info("=== logRepository 종료 ===");
     }
 
+    // 로그 저장 중 예외가 발생했을 때 회원 가입에 영향을 주지 않도록, 로그에서 발생하는 예외는 잡기로 하는 시나리오
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("=== memberRepository 호출 시작 ===");
+        memberRepository.save(member);
+        log.info("=== memberRepository 호출 종료 ===");
+        log.info("=== logRepository 시작 ===");
+        try {
+            logRepository.save(logMessage);
+        }
+        catch (RuntimeException e) {
+            log.info("로그 저장에 실패했습니다. logMesage = {}", logMessage);
+            log.info("정상 흐름 반환");
+        }
+        log.info("=== logRepository 종료 ===");
+    }
+
 }
